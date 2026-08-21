64e7863d9c34da794f2e4d9fad67571b31b2d597
gauravbchopra <41877234+gauravbchopra@users.noreply.github.com>
2026-08-21 18:27:35 -0400

    setup spring postgres

---
+++ src/main/java/com/healthcare/pharmacy/entity/Patient.java
@@ -0,0 +1,27 @@
+package com.healthcare.pharmacy.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="patient")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email_id", nullable = false, unique = true)
+    private String email;
+}
